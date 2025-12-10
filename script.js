4feb999e49fdbecc5730746abff8e63fe19053b0
AvocadoHead <55915461+AvocadoHead@users.noreply.github.com>
2025-12-10 22:52:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const items = [
-  { title: 'Podcasts', src: 'https://drive.google.com/file/d/1p4-uxcq4MLuf39U1MTsek5pEHE40Zh4N/view?usp=drive_link', link: 'pages/image.html' },
-  { title: 'Video | Simulation', src: 'https://drive.google.com/file/d/1JssQiFqnjA96ozChTomDaTaCSzQtY9-6/view?usp=drive_link', link: 'pages/simulation.html' },
-  { title: 'Video', src: 'https://drive.google.com/file/d/1leLgmJl99J-VvI_ERK9g3WlgUQHA_qtt/view?usp=drive_link', link: 'pages/video.html' },
-  { title: 'Personal Work', src: 'https://drive.google.com/file/d/17PuWeEYspDq6wybgCTc7tQWwa_3w2g_2/view?usp=drive_link', link: 'pages/personal-work.html' },
-  { title: 'Satire', src: 'https://drive.google.com/file/d/1xZwHCbZdBdiyR1z4RBXjUpGDxl2VOG51/view?usp=drive_link', link: 'pages/satire.html' }
+  { title: 'Podcasts', src: 'https://drive.google.com/file/d/1p4-uxcq4MLuf39U1MTsek5pEHE40Zh4N/view?usp=drive_link', link: 'pages/podcasts.html' },
+  { title: 'Video | Simulation', src: 'https://drive.google.com/file/d/1JssQiFqnjA96ozChTomDaTaCSzQtY9-6/view?usp=drive_link', link: 'pages/video-simulation.html' },
+  { title: 'Songs', src: 'https://drive.google.com/file/d/1Bx08pEL47s6mATgeDt2EP-epJq5fas5l/view?usp=drive_link', link: 'pages/songs.html' },
+  { title: 'Personal Work', src: 'https://drive.google.com/file/d/1PZOsdtXAuLlleN8BRbRZ-vIl0WZkd8sw/view?usp=drive_link', link: 'pages/personal-work.html' },
+  { title: 'Satire', src: 'https://drive.google.com/file/d/1tOWZ4QLOdw5oSVI5vlN9qkF118JYL4_2/view?usp=drive_link', link: 'pages/satire.html' }
 ];
 
 const carousel = document.getElementById('carousel');
@@ -229,5 +229,6 @@ window.addEventListener('load', () => {
   updateCarousel();
   startFloatingQuotes();
 });
+
 
 
